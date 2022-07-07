da40d30e4a9b8599ee838e114cee9982aeb575f0
yangjianxiang <yangjianxiang@xunyou.com>
2022-07-07 14:56:28 +0800

    log

--- packages/hi_core/lib/hi_core.dart
+++ packages/hi_core/lib/hi_core.dart
@@ -1,7 +1,5 @@
 library hi_core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:dartx/dartx.dart';
+
+export 'src/hi_logger.dart';
