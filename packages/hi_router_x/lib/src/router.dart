d89538b5a518a142dd47c893c41f53afdd04be99
yangjianxiang <yangjianxiang@xunyou.com>
2022-07-25 15:19:50 +0800

    hi_dialog

--- packages/hi_router_x/lib/src/router.dart
+++ packages/hi_router_x/lib/src/router.dart
@@ -35,14 +35,14 @@ class HiRouter {
       _router.define(path,
           handler: Handler(type: type.rawValue, handlerFunc: func));
 
-  void navigateTo(BuildContext context, String path,
+  Future navigateTo(BuildContext context, String path,
           {HiTransitionType? transition}) =>
       _router.navigateTo(context, path, transition: transition?.rawValue);
 
-  void push(BuildContext context, String path) => _router
+  Future push(BuildContext context, String path) => _router
       .navigateTo(context, path, transition: HiTransitionType.push.rawValue);
 
-  void present(BuildContext context, String path) => _router
+  Future present(BuildContext context, String path) => _router
       .navigateTo(context, path, transition: HiTransitionType.present.rawValue);
 
   void navigateBack<T>(BuildContext context, [T? result]) =>
